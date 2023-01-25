4cbb6c04f8bdd99a67c3e37a46f7c00e3fbd4299
krimou2 <krimouachouri21@gmail.com>
2023-01-25 19:01:47 +0000

    my first commit

--- app.py
+++ app.py
@@ -17,19 +17,28 @@ def index():
             model="text-davinci-003",
             prompt=generate_prompt(description, age, characters),
             temperature=0.4,
-            max_tokens=500
+            max_tokens=1000
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        res = response.choices[0].text
+        resl = slice(0, 350)
+        resl1 = slice(350, 500)
+        resl2 = slice(500, 650)
+        result = res[resl]
+        result1 = res[resl1]
+        result2 = res[resl2]
+        return redirect(url_for("about", result=result, result1=result1, result2=result2))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
-@app.route("/about")
+
+@ app.route("/about")
 def about():
     result = request.args.get("result")
-    return render_template("about.html", result=result)
+    result1 = request.args.get("result1")
+    result2 = request.args.get("result2")
+    return render_template("inde.html", result=result, result1=result1, result2=result2)
+
 
 def generate_prompt(description, age, characters):
     return ' Kids story.' + f'Story about: {description}' + f' target age: {age}' + f'Include characters: {characters}'
-
-    
