c7bebe3db297536e50e3930c6ae26d6657ba4b9a
krimou2 <krimouachouri21@gmail.com>
2023-01-26 12:08:38 +0000

    2nd commit

--- app.py
+++ app.py
@@ -17,16 +17,18 @@ def index():
             model="text-davinci-003",
             prompt=generate_prompt(description, age, characters),
             temperature=0.4,
-            max_tokens=1000
+            max_tokens=3500
         )
         res = response.choices[0].text
-        resl = slice(0, 350)
-        resl1 = slice(350, 500)
-        resl2 = slice(500, 650)
+        resl = slice(0, 500)
+        resl1 = slice(500, 1000)
+        resl2 = slice(1500, 2000)
+        resl3 = slice(2000, 2500)
         result = res[resl]
         result1 = res[resl1]
         result2 = res[resl2]
-        return redirect(url_for("about", result=result, result1=result1, result2=result2))
+        result3 = res[resl3]
+        return redirect(url_for("about", result=result, result1=result1, result2=result2, result3=result3))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
@@ -37,7 +39,8 @@ def about():
     result = request.args.get("result")
     result1 = request.args.get("result1")
     result2 = request.args.get("result2")
-    return render_template("inde.html", result=result, result1=result1, result2=result2)
+    result3 = request.args.get("result3")
+    return render_template("inde.html", result=result, result1=result1, result2=result2, result3=result3)
 
 
 def generate_prompt(description, age, characters):
