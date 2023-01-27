ea0e24ea4a9c5b9f1529b82b49e04de16f19ec8f
krimou2 <krimouachouri21@gmail.com>
2023-01-27 23:53:10 +0000

    4th commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-
+import re
 import openai
+from gtts import gTTS
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
@@ -13,21 +14,29 @@ def index():
         description = request.form["description"]
         age = request.form["age"]
         characters = request.form["characters"]
+
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=generate_prompt(description, age, characters),
             temperature=0.4,
-            max_tokens=3500
+            max_tokens=1000
         )
+
         res = response.choices[0].text
-        resl = slice(0, 500)
-        resl1 = slice(500, 1000)
-        resl2 = slice(1500, 2000)
-        resl3 = slice(2000, 2500)
+        resl = slice(0, 250)
+        resl1 = slice(250, 500)
+        resl2 = slice(500, 750)
+        resl3 = slice(750, 1000)
         result = res[resl]
         result1 = res[resl1]
         result2 = res[resl2]
         result3 = res[resl3]
+
+        def read_text(res):
+            tts = gTTS(res=res, lang='en')
+            tts.save("audio.mp3")
+            os.system("mpg321 audio.mp3")
+
         return redirect(url_for("about", result=result, result1=result1, result2=result2, result3=result3))
 
     result = request.args.get("result")
