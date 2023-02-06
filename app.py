a2a0d15400968cd8c3edf1f21f6d23bda7531db2
Joe Milton <zjoemilton@gmail.com>
2023-02-06 15:02:41 +0000

    updated key value

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, redirect, render_template, request, url_for, Response
 
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPEN_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 @app.route("/", methods=("GET", "POST"))
